7643b54974efd77c2a815bd261b31461a6b81127
rhwjdqja123 <127467312+rhwjdqja123@users.noreply.github.com>
2024-01-02 19:49:06 +0900

    Title: 출석 부르기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240102/출석 부르기/calling-attendance.java
+++ 240102/출석 부르기/calling-attendance.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in)
+        Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
 
         if (a==1){
