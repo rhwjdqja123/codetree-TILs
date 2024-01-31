e847b966e687ccfd322a7ed5fa737fb78d7f241b
rhwjdqja123 <127467312+rhwjdqja123@users.noreply.github.com>
2024-01-31 17:49:07 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240131/알파벳에 따른 평가/evaluation-by-alphabet.java
+++ 240131/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -9,6 +9,7 @@ public class Main {
             System.out.println("Superior");
         } else if (a == 'A') {
             System.out.println("Excellent");
+        }
              else if (a == 'B') {
             System.out.println("Good");
              }
