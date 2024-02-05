a243d4c5f512556078a158f0c3ff98bfc4506453
rhwjdqja123 <127467312+rhwjdqja123@users.noreply.github.com>
2024-02-05 20:20:15 +0900

    Title: 세 정수의 최솟값, Time: 200ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240205/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let mlnNunber;
+if (a<= b && a<= c)
+   minNumber = a;
+else if(b<= a && b<= c)
+   minNumber = b;
+
+else 
+   minNumber = c;
+     
+
+console.log(minNumber);
