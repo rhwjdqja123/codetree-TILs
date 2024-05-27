219c4ec8f7e14cc8d670a7a04808058d8fffa199
rhwjdqja123 <127467312+rhwjdqja123@users.noreply.github.com>
2024-05-27 17:10:23 +0900

    Title: 굉장한 숫자, Time: 221ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240527/굉장한 숫자/amazing-number.js
+++ 240527/굉장한 숫자/amazing-number.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 let a  = fs.readFileSync(0).toString().trim()
 
-if (a %2 ===0 || (a%2 ===1 && a>=10)){
-    console.log("special")
+if (a %2 ===1 && a%3 ===1  || (a%2 ===0 && a%5)){
+    console.log("true")
 }
 else{
-    console.log("normal");
+    console.log("false");
 }
