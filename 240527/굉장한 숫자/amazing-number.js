de8478fa538b09b08a42d136023c5abad55bb949
rhwjdqja123 <127467312+rhwjdqja123@users.noreply.github.com>
2024-05-27 17:12:45 +0900

    Title: 굉장한 숫자, Time: 231ms, Memory: 14MB, Status: Passed - CodeTree

--- 240527/굉장한 숫자/amazing-number.js
+++ 240527/굉장한 숫자/amazing-number.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let a  = fs.readFileSync(0).toString().trim()
 
-if (a %2 ===1 && a%3 ===1  || (a%2 ===0 && a%5)){
+if (a %2 ===1 && a%3 ===0  || (a%2 ===0 && a%5===0)){
     console.log("true")
 }
 else{
